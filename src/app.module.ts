0b5da453bd31b7b7a0ef3fb8690222531d754021
William Rodrigues <williamrodrigues2442@gmail.com>
2022-10-24 14:52:37 -0300

    chore: creates project structure

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = parseInt(process.env.APP_PORT) || 3001;
+  
+  await app.listen(PORT, () =>
+    console.log(`Server listening on port ⚡ ${PORT}`),
+  );
+}
+bootstrap();
