f3b374a87a1a8480d6d11a8403720db84d576527
William Rodrigues <williamrodrigues2442@gmail.com>
2022-10-29 17:26:19 -0300

    test: creates e2e tests for signup, signin

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 import { DatabaseService } from '../src/database/database.service';
 import { AppModule } from '../src/app.module';
+import { SignInAuthDto, SignUpAuthDto } from 'src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -12,6 +14,7 @@ describe('App e2e', () => {
       imports: [AppModule],
     }).compile();
 
+    const PORT = 3001;
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(
       new ValidationPipe({
@@ -19,9 +22,12 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(PORT);
 
     databaseService = app.get(DatabaseService);
     await databaseService.cleanDbInOrder();
+
+    pactum.request.setBaseUrl(`http://localhost:${PORT}`);
   });
 
   afterAll(async () => {
@@ -30,17 +36,112 @@ describe('App e2e', () => {
   });
 
   describe('Auth', () => {
+    const signUpDto: SignUpAuthDto = {
+      email: 'test@gmail.com',
+      password: 'test123',
+      firstName: 'Test',
+      lastName: 'Test',
+    };
+    const signInDto: SignInAuthDto = {
+      email: signUpDto.email,
+      password: signUpDto.password,
+    };
+
     describe('Signup', () => {
-      it.todo('Should signup');
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withJson({
+            password: signUpDto.password,
+          })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withJson({
+            email: signUpDto.email,
+          })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should throw if no body', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should signup', async () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withJson(signUpDto)
+          .expectStatus(HttpStatus.CREATED);
+      });
     });
 
     describe('Signin', () => {
-      it.todo('Should signin');
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withJson({
+            password: signInDto.password,
+          })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should throw if password empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withJson({
+            email: signInDto.email,
+          })
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should throw if no body', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .expectStatus(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should signin', async () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withJson(signInDto)
+          .expectStatus(HttpStatus.OK)
+          .stores('userAt', 'access_token');
+      });
     });
   });
 
   describe('User', () => {
-    describe('Get me', () => {});
+    describe('Get me', () => {
+      it('should throw if not authenticated', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .expectStatus(HttpStatus.UNAUTHORIZED);
+      });
+
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(HttpStatus.OK);
+      });
+    });
 
     describe('Edit user', () => {});
 
@@ -54,8 +155,8 @@ describe('App e2e', () => {
 
     describe('Get bookmark by id', () => {});
 
-    describe('Edit bookmark', () => {});
+    describe('Edit bookmark by id', () => {});
 
-    describe('Delete bookmark', () => {});
+    describe('Delete bookmark by id', () => {});
   });
 });
