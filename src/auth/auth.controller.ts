8694235be7fe50a69ab025525e60afe4aeb1d351
William Rodrigues <williamrodrigues2442@gmail.com>
2022-10-28 22:19:47 -0300

    hotfix: returns status code 200 when sign in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInAuthDto, SignUpAuthDto } from './dto';
 
@@ -6,6 +6,7 @@ import { SignInAuthDto, SignUpAuthDto } from './dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   async signIn(@Body() dto: SignInAuthDto) {
     return await this.authService.doSignIn(dto);
