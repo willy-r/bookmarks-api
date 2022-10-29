428d62ed684ccba5c5f615464410d6bc3bea2b4a
William Rodrigues <williamrodrigues2442@gmail.com>
2022-10-28 21:54:47 -0300

    feat: creates route to user access their informations

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { DatabaseService } from '../../database/database.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private databaseService: DatabaseService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
     });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.databaseService.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+    delete user.hashPassword;
+    return user;
   }
 }
